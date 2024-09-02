c00e8c3d4e0c1d45995c2b30f86cb7604d4a8623
root <root@user-puser>
2024-09-02 09:46:47 +0700

    Refactor history page output: switched from list to table format

--- renderer.php
+++ renderer.php
@@ -5,7 +5,6 @@ defined('MOODLE_INTERNAL') || die();
 
 class block_quadratic_calculator_renderer extends plugin_renderer_base
 {
-
     /**
      * Рендерит страницу истории.
      *
@@ -21,16 +20,48 @@ class block_quadratic_calculator_renderer extends plugin_renderer_base
         $output .= html_writer::tag('h2', get_string('history', 'block_quadratic_calculator'));
 
         if (!empty($records['records'])) {
-            $output .= html_writer::start_tag('ul');
+            $output .= html_writer::start_tag('table', ['class' => 'generaltable fullwidth']);
+            $output .= html_writer::start_tag('thead');
+            $output .= html_writer::start_tag('tr');
+            $output .= html_writer::tag('th', "a", ['scope' => 'col']);
+            $output .= html_writer::tag('th', "b", ['scope' => 'col']);
+            $output .= html_writer::tag('th', "c", ['scope' => 'col']);
+            $output .= html_writer::tag('th', "x1", ['scope' => 'col']);
+            $output .= html_writer::tag('th', "x2", ['scope' => 'col']);
+            $output .= html_writer::end_tag('tr');
+            $output .= html_writer::end_tag('thead');
+            $output .= html_writer::start_tag('tbody');
+
             foreach ($records['records'] as $record) {
-                $output .= html_writer::tag('li', "a={$record->a}, b={$record->b}, c={$record->c}, x1={$record->x1}, x2={$record->x2}");
+                $output .= $this->render_history_row($record);
             }
-            $output .= html_writer::end_tag('ul');
+
+            $output .= html_writer::end_tag('tbody');
+            $output .= html_writer::end_tag('table');
         } else {
             $output .= html_writer::tag('p', get_string('nohistory', 'block_quadratic_calculator'));
         }
 
         $output .= html_writer::end_tag('div');
+        return $output;
+    }
+
+    /**
+     * Рендерит строку таблицы истории.
+     *
+     * @param stdClass $record Запись с данными для отображения.
+     * @return string строка таблицы HTML код.
+     */
+    private function render_history_row(stdClass $record): string
+    {
+        $output = html_writer::start_tag('tr', ['class' => 'r' . ($record->id % 2)]);
+        $output .= html_writer::tag('td', $record->a);
+        $output .= html_writer::tag('td', $record->b);
+        $output .= html_writer::tag('td', $record->c);
+        $output .= html_writer::tag('td', $record->x1);
+        $output .= html_writer::tag('td', $record->x2);
+        $output .= html_writer::end_tag('tr');
+
         return $output;
     }
 }
--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_quadratic_calculator';
-$plugin->version = 2024090101;
+$plugin->version = 2024090209;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = '1.0';
