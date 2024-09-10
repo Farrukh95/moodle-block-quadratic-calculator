475d5ef7cd2224f87de4dc8b98ec4961c0a0a4db
farrukh <pfd@tspu.edu.ru>
2024-09-11 03:18:01 +0700

    changed CONTEXT_BLOCK to CONTEXT_SYSTEM

--- db/access.php
+++ db/access.php
@@ -3,7 +3,7 @@ $capabilities = array(
     'block/quadratic_calculator:addinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
         'captype' => 'write',
-        'contextlevel' => CONTEXT_BLOCK,
+        'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
@@ -13,9 +13,10 @@ $capabilities = array(
     'block/quadratic_calculator:myaddinstance' => array(
         'riskbitmask' => RISK_SPAM | RISK_XSS,
         'captype' => 'write',
-        'contextlevel' => CONTEXT_BLOCK,
+        'contextlevel' => CONTEXT_SYSTEM,
         'archetypes' => array(
             'user' => CAP_ALLOW
         ),
     ),
 );
+
