681a234e212be46d91825eb83340fb1e2a683d48
farrukh <pfd@tspu.edu.ru>
2024-09-11 03:27:42 +0700

    Update version.php to version 2024091000

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_quadratic_calculator';
-$plugin->version = 2024090209;
+$plugin->version = 2024091000;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = '1.0';
