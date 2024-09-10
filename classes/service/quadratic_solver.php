1a60161ff196be60115d50f8ebd1429b60813417
farrukh <pfd@tspu.edu.ru>
2024-09-11 03:26:51 +0700

    Enhance quadratic_solver to handle linear equations and improve error handling

--- classes/service/quadratic_solver.php
+++ classes/service/quadratic_solver.php
@@ -7,15 +7,38 @@ class quadratic_solver
 {
     public function solve(float $a, float $b, float $c): array
     {
+        if ($a == 0) {
+            // Линейное уравнение
+            return $this->solveLinear($b, $c);
+        } else {
+            // Квадратное уравнение
+            return $this->solveQuadratic($a, $b, $c);
+        }
+    }
+
+    private function solveLinear(float $b, float $c): array
+    {
+        if ($b == 0) {
+            return ['x1' => null, 'x2' => null];
+        } else {
+            $x1 = -$c / $b;
+            return ['x1' => $x1, 'x2' => null];
+        }
+    }
+
+    private function solveQuadratic(float $a, float $b, float $c): array
+    {
         $discriminant = $b * $b - 4 * $a * $c;
 
         if ($discriminant < 0) {
-            throw new \moodle_exception('Реальных корней нет!');
+            return ['x1' => null, 'x2' => null];
+        } else {
+            $sqrtDiscriminant = sqrt($discriminant);
+            $x1 = (-$b + $sqrtDiscriminant) / (2 * $a);
+            $x2 = (-$b - $sqrtDiscriminant) / (2 * $a);
+
+            return ['x1' => $x1, 'x2' => $x2];
         }
-
-        $x1 = (-$b + sqrt($discriminant)) / (2 * $a);
-        $x2 = (-$b - sqrt($discriminant)) / (2 * $a);
-
-        return ['x1' => $x1, 'x2' => $x2];
     }
+
 }
