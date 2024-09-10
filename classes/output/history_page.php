327ff7519f908e7641f568bc88f95586b38f035a
farrukh <pfd@tspu.edu.ru>
2024-09-11 03:23:30 +0700

    modify history_page

--- classes/output/history_page.php
+++ classes/output/history_page.php
@@ -3,13 +3,9 @@ declare(strict_types=1);
 
 namespace block_quadratic_calculator\output;
 
-use renderable;
-use renderer_base;
-use templatable;
-
-class history_page implements renderable, templatable
+class history_page implements \renderable
 {
-    public function export_for_template(renderer_base $output)
+    public function export_for_template(\renderer_base $output)
     {
         global $DB, $USER;
         $records = $DB->get_records('block_quadratic_calculator', ['userid' => $USER->id], 'created_at DESC');
